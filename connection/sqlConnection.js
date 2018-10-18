92553fcd7f46e74b953bf3b5e126abab825016ba
brightroots09 <brightroots09@brightroots.net>
2018-10-18 12:11:18 +0530

    Added attendance edit route to admin

--- connection/sqlConnection.js
+++ connection/sqlConnection.js
@@ -1,26 +1,26 @@
 const mysql = require('mysql');
 
 global.con = mysql.createConnection({
-    "host": "127.0.0.1",
-    "port": 3306,
-    "database": "erp",
-    "password": "root",
-    "user": "root",
-    "connector": "mysql",
-    "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
-    //  host : 'localhost',
-    //  user : 'brightde_erp',
-    //  password : 'cb5A?NERYalc',
-    //  database : 'brightde_erp'
+    // "host": "127.0.0.1",
+    // "port": 3306,
+    // "database": "erp",
+    // "password": "root",
+    // "user": "root",
+    // "connector": "mysql",
+    // "socketPath": "/Applications/MAMP/tmp/mysql/mysql.sock"
+     host : 'localhost',
+     user : 'brightde_erp',
+     password : 'cb5A?NERYalc',
+     database : 'brightde_erp'
 });
 
 let connection = async function () {
     try {
         await con.connect();
-        await con.query(`create database if not exists erp`);
-        await con.query(`use erp`);
-        // await con.query(`create database if not exists brightde_erp`);
-        // await con.query(`use brightde_erp`);
+        // await con.query(`create database if not exists erp`);
+        // await con.query(`use erp`);
+        await con.query(`create database if not exists brightde_erp`);
+        await con.query(`use brightde_erp`);
 
         console.log("Database created");
         console.log("Connected to SQL");
